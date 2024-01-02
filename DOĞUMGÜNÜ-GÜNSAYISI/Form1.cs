00c4070b3041b16a85adbd067b91963e858a7c07
elfi <145452036+elifftorun@users.noreply.github.com>
2024-01-02 14:10:10 +0300

    Add files via upload

---
+++ DOĞUMGÜNÜ-GÜNSAYISI/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DOĞUMGÜNÜ_GÜNSAYISI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dogumTarihi=new DateTime(2024,8,8);
+            DateTime bugun = DateTime.Today;
+            TimeSpan kalansure = dogumTarihi-bugun;
+            int kalangun=kalansure.Days;
+            label1.Text = kalangun.ToString();
+        }
+    }
+}
